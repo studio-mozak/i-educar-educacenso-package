b4c9a3705569b1442b17b90adc171f016f4acc37
tiagoCBMozak <thiago.bezerra@studiomozak.com.br>
2026-03-12 22:09:23 -0300

    Update EducacensoImportJob.php

--- src/Jobs/EducacensoImportJob.php
+++ src/Jobs/EducacensoImportJob.php
@@ -7,13 +7,15 @@ use DateTime;
 use iEducar\Packages\Educacenso\Services\ImportServiceFactory;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
 use Throwable;
 
-class EducacensoImportJob implements ShouldQueue
+class EducacensoImportJob implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable;
     use InteractsWithQueue;
@@ -40,13 +42,18 @@ class EducacensoImportJob implements ShouldQueue
      */
     private $registrationDate;
 
+    /**
+     * Tempo máximo de execução do job
+     */
     public $timeout = 3600;
 
     /**
+     * Tempo que o job permanece único na fila
+     */
+    public $uniqueFor = 3600;
+
+    /**
      * Create a new job instance.
-     *
-     * @param string                $databaseConnection
-     * @param DateTime              $registrationDate
      */
     public function __construct(EducacensoImportModel $educacensoImport, $importArray, $databaseConnection, $registrationDate)
     {
@@ -57,25 +64,51 @@ class EducacensoImportJob implements ShouldQueue
     }
 
     /**
+     * Define o identificador único do job
+     */
+    public function uniqueId()
+    {
+        return 'educacenso-import-' . $this->educacensoImport->id;
+    }
+
+    /**
+     * Middleware para evitar execução paralela
+     */
+    public function middleware()
+    {
+        return [
+            (new WithoutOverlapping($this->educacensoImport->id))
+                ->expireAfter(3600)
+        ];
+    }
+
+    /**
      * Execute the job.
-     *
-     * @return void
      *
      * @throws Throwable
      */
     public function handle(): void
     {
         ini_set('memory_limit', '1G');
+
         DB::setDefaultConnection($this->databaseConnection);
         DB::beginTransaction();
 
         try {
-            $importService = ImportServiceFactory::createImportService($this->educacensoImport->year, $this->registrationDate);
-            $importService->import($this->importArray, $this->educacensoImport->user);
+            $importService = ImportServiceFactory::createImportService(
+                $this->educacensoImport->year,
+                $this->registrationDate
+            );
+
+            $importService->import(
+                $this->importArray,
+                $this->educacensoImport->user
+            );
+
             $importService->adaptData();
+
         } catch (Throwable $e) {
             DB::rollBack();
-
             throw $e;
         }
 
@@ -86,6 +119,9 @@ class EducacensoImportJob implements ShouldQueue
         DB::commit();
     }
 
+    /**
+     * Tags para o Horizon
+     */
     public function tags()
     {
         return [
